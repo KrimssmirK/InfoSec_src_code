299ab29f311aeee03dbdfae4c44c78a134281e73
KrimssmirK <krimssmirk003@gmail.com>
2022-12-18 23:58:50 +0800

    setup environment

--- index.php
+++ index.php
@@ -1,144 +1,174 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">	
+	<title>BlogSite</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
 
-    <!-- Bootstrap v5.0 -->
-    <link 
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" 
-      rel="stylesheet" 
-      integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-      crossorigin="anonymous"
-    >
+<body>
 
-    <!-- to support viewport for small devices such as phone, tablets and so on -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BLOG</title>
-  </head>
-  <body>
-    <div class="container mt-5">
-      <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
+	<header>
+	  <div class="collapse bg-dark" id="navbarHeader">
+	    <div class="container">
+	      <div class="row">
+	        <div class="col-sm-8 col-md-7 py-4">
+	          <h4 class="text-white">About Course</h4>
+	          <p class="text-muted">Include course description here...</p>
+	        </div>
+	        <div class="col-sm-4 offset-md-1 py-4">
+	          <h4 class="text-white">Sites</h4>
+	          <ul class="list-unstyled">
+	            <li><a href="https://national-u.edu.ph/" class="text-white">Visit Official Website</a></li>
+                <li><a href="https://www.facebook.com/NationalUniversityPhilippines" class="text-white">Follow on Facebook - National U</a></li>
+	            <li><a href="https://www.facebook.com/groups/ccitofficial/" class="text-white">Follow on Facebook - NUCCIT</a></li>
+	          </ul>
+	        </div>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="navbar navbar-dark bg-dark shadow-sm">
+	    <div class="container">
+	      <a href="#" class="navbar-brand d-flex align-items-center">
+	        <strong>NUCCIT Blog Site</strong>
+	      </a>
+	      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="navbar-toggler-icon"></span>
+	      </button>
+	    </div>
+	  </div>
+	</header>
+
+
+	<main>
+	  <section class="py-5 text-center container">
+      <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="./img/i1.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Breaking the scaling limits of analog computing</h5>
-              <p>New technique could diminish errors that hamper the performance of super-fast analog optical neural networks.</p>
+            <div class="carousel-item active">
+            <img src="images/carousel1.jpg" class="d-block w-100" alt="...">
             </div>
-          </div>
-          <div class="carousel-item">
-            <img src="./img/i2.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>The task of magnetic classification suddenly looks easier</h5>
-              <p>MIT undergraduate researchers Helena Merker, Harry Heiberger, and Linh Nguyen, and PhD student Tongtong Liu, exploit machine-learning techniques to determine the magnetic structure of materials.</p>
+            <div class="carousel-item">
+            <img src="images/carousel2.jpg" class="d-block w-100" alt="...">
             </div>
-          </div>
-          <div class="carousel-item">
-            <img src="./img/i3.png" class="d-block w-100" alt="...">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Study urges caution when comparing neural networks to the brain</h5>
-              <p>Computing systems that appear to generate brain-like activity may be the result of researchers guiding them to a specific outcome.</p>
-            </div>
-          </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
         </button>
-      </div>
-      <div class="row mt-5">
-        <div class="col-8">
-          <div class="card"">
-            <img src="./img/ml.png" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">Recent Machine Learning Algorithms</h5>
-              <p class="card-text">Different types of ML Algorithms that are used nowadays.</p>
-              <a href="#" class="btn btn-primary">Explore</a>
-            </div>
-          </div>
         </div>
-        <div class="col">
-          <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Email address</label>
-            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="email@example.com">
-          </div>
-          <div class="mb-3">
-            <label for="password" class="form-label">Password</label>
-            <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password">
-          </div>
-          <div class="d-grid gap-2">
-            <button class="btn btn-primary" type="button">Sign In</button>
-            <a href="./register.php" class="btn btn-outline-primary" type="button">Register</a>
-          </div>
-    
-          <div class="mb-3">
-            <label for="exampleFormControlTextarea1" class="form-label">Post Comment</label>
-            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            <button type="button" class="btn btn-primary mt-2">Submit</button>
-          </div>
-        </div>
-      </div>
-      <h2>Comments</h2>      
-  <table class="table table-borderless">
-    <thead>
-      <tr>
-        <th>Message</th>
-        <th>Post Date</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Cras justo odio. dapibus ac faciisis in. egestas eget quam.</td>
-        <td>20 Jan 2016</td>
-      </tr>
-      <tr>
-        <td>Cras justo odio. dapibus ac faciisis in. egestas eget quam.</td>
-        <td>08 Feb 2011</td>
-      </tr>
-      <tr>
-        <td>Cras justo odio. dapibus ac faciisis in. egestas eget quam.</td>
-        <td>20 Jan 2016</td>
-      </tr>
-    </tbody>
-  </table>
-      <div class="d-flex justify-content-center">
-        <nav aria-label="Page navigation example  ">
-          <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-          </ul>
-        </nav>
-      </div>
+	  </section>
 
+	  <section class="py-2 bg-light">
+	  	<div class="container">
+	  		<div class="row">
 
+	  			<div class="col-md-8">
+	  				<div class="card shadow-sm">
+		          	<img src="images/INFOSEC.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                        <h1 class="display-5">What is information security?</h1>
+                        <figure>
+                            <blockquote class="blockquote">
+                                <p style="text-align: justify; text-justify: inter-word;">
+                                    Information security, often shortened to infosec, is the practice, policies and principles to protect digital data and other kinds of information. infosec responsibilities include establishing a set of business processes that will protect information assets, regardless of how that information is formatted or whether it is in transit, is being processed or is at rest in storage.
+                                    Generally, an organization applies information security to guard digital information as part of an overall cybersecurity program. infosec's three primary principles, called the CIA triad, are confidentiality, integrity and availability.
+                                    In short, infosec is how you make sure your employees can get the data they need, while keeping anyone else from accessing it. It can also be associated with risk management and legal regulations.
+                                </p>
+                            </blockquote>
+                            <figcaption class="blockquote-footer">
+                                Someone famous in <cite title="Source Title">TechTarget</cite>
+                                <small class="text-muted"><a href="https://www.techtarget.com/searchsecurity/definition/information-security-infosec">Source</a></small>
+                            </figcaption>
+                        </figure>
+		                </div>
+		          </div>
+	  			</div>
 
-      
+	  			<div class="col-md-4">
+	  				<div class="row">
+						<form action="" class="form-control" id="frmLogin" enctype="multipart/form-data" autocomplete="off">
+							<input type="hidden" class="form-control" id="form_name" name="form_name" value="frmLogin">
+							<h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
-    </div>
-    
+							<div class="mb-3">
+								<label for="email" class="form-label">Email</label>
+								<input type="email" class="form-control" id="email" name="email">
+							</div>
 
-    
+							<div class="mb-3">
+								<label for="pass" class="form-label">Password</label>
+								<input type="password" class="form-control" id="pass" name="pass">
+							</div>
 
-     
+							<div class="d-grid gap-2">
+								<button class="btn btn-lg btn-primary" type="submit">Sign in</button>
+								<a type='button' class='btn btn-lg btn-info' href='ui_register.php'>Register Here</a>
+							</div>
 
-  </body>
+						</form>
+					</div>
 
-  <!-- to be able to use the functionalities of Bootstrap -->
-  <script 
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
-    crossorigin="anonymous">
-  </script>
+					<div class="row" style="margin-top: 5px;">
+						<form action="" class="form-control" id="frmComments" name="frmComments" enctype="multipart/form-data" autocomplete="off">
+						
+					    <h1 class="h3 mb-3 fw-normal">Post Comments</h1>
+
+					    <div class="mb-3">
+						    <textarea id="comment" name="comment" style="width:100%; height: 228px;"></textarea>
+						</div>
+
+					   <button class="btn btn-lg btn-primary" type="submit">Submit Post</button>
+					    					    
+					  </form>
+					</div>
+
+	  			</div>
+
+	  		</div>
+	  	</div>
+	  </section>
+
+	  <section class="py-5">
+	  	<div class="container">
+	  		<div class="row">
+	  			<div class="col-md-12">
+	  				<h1 class="h3 mb-3 fw-normal">Comments</h1>
+	  				<table class="table table-striped table-responsive">
+                      <thead>
+                        <tr>
+                        <th scope="col">Message</th>
+                        <th scope="col">Post Date</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Static Message</td>
+                            <td>October 31, 2022</td>
+                        </tr>
+                        <tr>
+                            <td>Static Message</td>
+                            <td>October 31, 2022</td>
+                        </tr>
+                        <tr>
+                            <td>Static Message</td>
+                            <td>October 31, 2022</td>
+                        </tr>
+                    </tbody>
+
+                    </table>
+	  			</div>
+	  		</div>	
+	  	</div>
+	  </section>
+
+	</main>
+	<script src="js/bootstrap.min.js"></script>
+</body>
+
 </html>
