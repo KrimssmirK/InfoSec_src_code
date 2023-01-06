d043011a42026040e3920d1899ecb0b6e80ec690
KrimssmirK <krimssmirk003@gmail.com>
2023-01-06 12:06:51 +0800

    added signin functionality

---
+++ signin.php
@@ -0,0 +1,39 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // collect value of input field
+    $email = $_POST['email'];
+    $pswd = $_POST['pass'];
+  }
+
+// config of database
+$servername = "localhost";
+$username = "mamp";
+$password = "b(Sp7k6[wv+#EG5";
+$dbname = "InfoSec_Sugino";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+  
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT Email, Password FROM tblAccounts WHERE Email = '$email'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  // output data of each row
+  while($row = mysqli_fetch_assoc($result)) {
+    if ($row['Email'] == $email && $row['Password'] == $pswd) {
+      echo 'Success Sign in';
+    } else {
+      echo 'Wrong username and password';
+    }
+  }
+} else {
+  echo "user does not exist";
+}
+  
+mysqli_close($conn);
+?>
