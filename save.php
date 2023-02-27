181ea885ca88f7ef9cf2d2bb8bd0d017abf6f883
KrimssmirK <krimssmirk003@gmail.com>
2023-02-27 22:35:33 +0800

    added comments on save.php

--- save.php
+++ save.php
@@ -1,22 +1,22 @@
-<?php 
-  
-  include_once 'config.php';
+<?php
+// this is the configuration about the database
+include_once 'config.php';
 
-  $id = $_POST['id'];
-  $name = $_POST['name'];
-  $email = $_POST['email'];
-  $pwd = $_POST['pswd'];
-  $newModifiedDate = date('Y-m-j');
+$id = $_POST['id'];
+$name = $_POST['name'];
+$email = $_POST['email'];
+$pwd = $_POST['pswd'];
+$newModifiedDate = date('Y-m-j');
 
-  $sql = "UPDATE $tblaccounts SET Name = '$name', Email = '$email', Password = '$pwd', ModifiedDate = '$newModifiedDate' WHERE ID = $id";
+$sql = "UPDATE $tblaccounts SET Name = '$name', Email = '$email', Password = '$pwd', ModifiedDate = '$newModifiedDate' WHERE ID = $id";
 
-  if (mysqli_query($conn, $sql)) {
-    // success
-    header("Location: http://localhost:8888/ui_manage_account.php");
-    die();
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-  mysqli_close($conn);
+if (mysqli_query($conn, $sql)) {
+  // success
+  header("Location: http://localhost:8888/ui_manage_account.php");
+  die();
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
