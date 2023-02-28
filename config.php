cef8fc77e1b0e2436f45328d5759c8f727b08a27
KrimssmirK <krimssmirk003@gmail.com>
2023-02-28 15:48:32 +0800

    added comments on config.php

--- config.php
+++ config.php
@@ -1,20 +1,37 @@
-<?php 
-  /*
-  These will be changed due to local configuration
-  */
-  $servername = "localhost";
-  $username = "mamp";
-  $password = "b(Sp7k6[wv+#EG5";
-  $dbname = "InfoSec_Sugino";
+<?php
 
-  $tblaccounts = "tblAccounts";
-  $tblcomments = "tblComments";
-    
-  // Create connection
-  $conn = mysqli_connect($servername, $username, $password, $dbname);
-    
-  // Check connection
-  if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-  } 
+/**
+ * config flow
+ * 1. connect to database
+ * 2. if fails show the error message
+ * 3. if not proceed with next operation
+ * 
+ * data
+ *  - database info (servername, username, password, dbname)
+ *  - table names (consistency)
+ */
+
+
+/*
+These will be changed due to local configuration
+*/
+
+// database configuration
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "infosec_sugino";
+
+
+// table names
+$tblaccounts = "tblaccounts";
+$tblcomments = "tblcomments";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
 ?>
