97115c247e6f1964208e5ec26b6fc7581d193135
KrimssmirK <krimssmirk003@gmail.com>
2023-03-02 16:03:41 +0800

    fixed error in manage account ui

--- ui_manage_account.php
+++ ui_manage_account.php
@@ -1,22 +1,23 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">	
-	<title>BlogSite</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>BlogSite</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/dashboard.css">
 
   <script src="js/jquery-3.6.0.min.js"></script>
   <script src="js/jquery-ui.min.js"></script>
-  <script src="js/jquery.validate.min.js"></script>   
+  <script src="js/jquery.validate.min.js"></script>
 </head>
 
 <body>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-        <img src="images/infosec.png" alt="Logo" width="120" height="24"/>
+        <img src="images/infosec.png" alt="Logo" width="120" height="24" />
       </a>
       <a class="btn btn-outline-success" href="index.php">Logout</a>
     </div>
@@ -36,7 +37,8 @@
             </li>
           </ul>
 
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+          <h6
+            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
             <span>DATA ADMINISTRATION</span>
           </h6>
           <ul class="nav flex-column mb-2">
@@ -55,13 +57,14 @@
           </ul>
         </div>
       </nav>
-     
+
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div
+          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">User Accounts Management</h1>
         </div>
-       
+
         <table class="table table-borderless">
           <thead>
             <tr>
@@ -73,31 +76,31 @@
             </tr>
           </thead>
           <tbody>
-            <?php 
-              include_once 'config.php';
-              $sql = "SELECT ID, Name, CreatedDate, ModifiedDate FROM $tblaccounts";
-              $result = mysqli_query($conn, $sql);
-              if (mysqli_num_rows($result) > 0) {
-                // output data of each row
-                while($row = mysqli_fetch_assoc($result)) {
-                  $orgCreatedDate = $row['CreatedDate'];
-                  $newCreatedDate = date("d M Y", strtotime($orgCreatedDate));
-                  $orgModifiedDate = $row['ModifiedDate'];
-                  $newModifiedDate = date("d M Y", strtotime($orgModifiedDate));
-                  echo '<tr>
-                          <td>'.$row['ID'].'</td>
-                          <td>'.$row['Name'].'</td>
-                          <td>'.$newCreatedDate.'</td>
-                          <td>'.$newModifiedDate.'</td>
+            <?php
+            include_once 'config.php';
+            $sql = "SELECT ID, Name, CreatedDate, ModifiedDate FROM $tbl_accounts";
+            $result = mysqli_query($conn, $sql);
+            if (mysqli_num_rows($result) > 0) {
+              // output data of each row
+              while ($row = mysqli_fetch_assoc($result)) {
+                $orgCreatedDate = $row['CreatedDate'];
+                $newCreatedDate = date("d M Y", strtotime($orgCreatedDate));
+                $orgModifiedDate = $row['ModifiedDate'];
+                $newModifiedDate = date("d M Y", strtotime($orgModifiedDate));
+                echo '<tr>
+                          <td>' . $row['ID'] . '</td>
+                          <td>' . $row['Name'] . '</td>
+                          <td>' . $newCreatedDate . '</td>
+                          <td>' . $newModifiedDate . '</td>
                           <td>
                             <form method="POST" action="edit.php">
-                              <input type="hidden" name="id" value="'.$row['ID'].'" />
+                              <input type="hidden" name="id" value="' . $row['ID'] . '" />
                               <button type="submit" class="btn btn-primary">Edit</button>
-                              <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModal-'.$row['ID'].'">Delete</button>
+                              <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#myModal-' . $row['ID'] . '">Delete</button>
                             </form>
                           </td>
                         </tr>
-                        <div class="modal" id="myModal-'.$row['ID'].'">
+                        <div class="modal" id="myModal-' . $row['ID'] . '">
                           <div class="modal-dialog">
                             <div class="modal-content">
 
@@ -116,7 +119,7 @@
                               <div class="modal-footer">
                               <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                                 <form action="delete_account.php" method="post">
-                                  <input type="hidden" name="id" value="'.$row['ID'].'"/>
+                                  <input type="hidden" name="id" value="' . $row['ID'] . '"/>
                                   <button type="submit" class="btn btn-danger" data-bs-dismiss="modal">Delete</button>
                                 </form>
                               </div>
@@ -124,18 +127,18 @@
                             </div>
                           </div>
                         </div>';
-                  }
-                }
-                
-                
+              }
+            }
+
+
             ?>
           </tbody>
         </table>
-       
+
       </main>
     </div>
   </div>
-    
+
   <script src="js/dashboard.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/feather.min.js"></script>
@@ -143,8 +146,9 @@
     feather.replace()
   </script>
 </body>
+
 </html>
 
-<?php 
-  mysqli_close($conn);
+<?php
+mysqli_close($conn);
 ?>
