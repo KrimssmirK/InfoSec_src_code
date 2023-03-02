b78748c9a35b4f73230961c57a8af24f5f336ba5
KrimssmirK <krimssmirk003@gmail.com>
2023-03-02 15:59:48 +0800

    changed the the value of name attribute in index.php

--- index.php
+++ index.php
@@ -154,7 +154,7 @@
 								</div>
 								<div class="mb-3">
 									<label for="password" class="form-label">Password</label>
-									<input type="password" class="form-control" id="pass" name="pass"
+									<input type="password" class="form-control" id="pass" name="password"
 										autocomplete=new-password required>
 									<!-- added not automatically set the password -->
 								</div>
