4ed40bdc482cb191e97e0251e53923da0ab980ed
KrimssmirK <krimssmirk003@gmail.com>
2023-03-02 16:03:23 +0800

    fixed error in edit.php

--- edit.php
+++ edit.php
@@ -1,22 +1,23 @@
 <!doctype html>
 <html lang="en">
+
 <head>
-	<meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">	
-	<title>BlogSite</title>
-	<link rel="stylesheet" href="css/bootstrap.min.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>BlogSite</title>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
   <link rel="stylesheet" href="css/dashboard.css">
 
   <script src="js/jquery-3.6.0.min.js"></script>
   <script src="js/jquery-ui.min.js"></script>
-  <script src="js/jquery.validate.min.js"></script>   
+  <script src="js/jquery.validate.min.js"></script>
 </head>
 
 <body>
   <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
     <div class="container-fluid">
       <a class="navbar-brand" href="#">
-        <img src="images/infosec.png" alt="Logo" width="120" height="24"/>
+        <img src="images/infosec.png" alt="Logo" width="120" height="24" />
       </a>
       <a class="btn btn-outline-success" href="index.php">Logout</a>
     </div>
@@ -36,7 +37,8 @@
             </li>
           </ul>
 
-          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
+          <h6
+            class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted text-uppercase">
             <span>DATA ADMINISTRATION</span>
           </h6>
           <ul class="nav flex-column mb-2">
@@ -55,54 +57,56 @@
           </ul>
         </div>
       </nav>
-     
+
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div
+          class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
           <h1 class="h2">Edit User Accounts</h1>
         </div>
-        <?php 
-          include_once 'config.php';
+        <?php
+        include_once 'config.php';
 
-          $id = $_POST['id'];
+        $id = $_POST['id'];
 
-          $sql = "SELECT Name, Email, Password FROM $tblaccounts where ID = '$id'";
-          $result = mysqli_query($conn, $sql);
-          $row = mysqli_fetch_assoc($result);
+        $sql = "SELECT Name, Email, Password FROM $tbl_accounts where ID = '$id'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result);
 
-          $name = $row['Name'];
-          $email = $row['Email'];
-          $password = $row['Password'];
+        $name = $row['Name'];
+        $email = $row['Email'];
+        $password = $row['Password'];
 
-          mysqli_close($conn);
+        mysqli_close($conn);
         ?>
         <form action="save.php" method="post">
-            <div class="mb-3 mt-3">
-                <label for="name" class="form-label">Name</label>
-                <input type="text" class="form-control" id="name" value="<?php echo $name; ?>" name="name" required>
-            </div>
-            <div class="mb-3 mt-3">
-                <label for="email" class="form-label">Email address</label>
-                <input type="email" class="form-control" id="email" value="<?php echo $email; ?>" name="email" required>
+          <div class="mb-3 mt-3">
+            <label for="name" class="form-label">Name</label>
+            <input type="text" class="form-control" id="name" value="<?php echo $name; ?>" name="name" required>
+          </div>
+          <div class="mb-3 mt-3">
+            <label for="email" class="form-label">Email address</label>
+            <input type="email" class="form-control" id="email" value="<?php echo $email; ?>" name="email" required>
             <div class="mb-3">
-                <label for="pwd" class="form-label">Password</label>
-                <input type="password" class="form-control" id="pwd" value="<?php echo $password; ?>" name="pswd" required>
+              <label for="pwd" class="form-label">Password</label>
+              <input type="password" class="form-control" id="pwd" value="<?php echo $password; ?>" name="pswd"
+                required>
             </div>
-            <input type="hidden" name="id" value="<?php echo $id; ?>"/>
+            <input type="hidden" name="id" value="<?php echo $id; ?>" />
             <hr>
             <div class="d-flex justify-content-end">
-                <a href="./ui_manage_account.php" type="submit" class="btn btn-secondary px-3 me-2">Cancel</a>
-                <button type="submit" class="btn btn-primary px-3" id="create">Save</button>
+              <a href="./ui_manage_account.php" type="submit" class="btn btn-secondary px-3 me-2">Cancel</a>
+              <button type="submit" class="btn btn-primary px-3" id="create">Save</button>
             </div>
         </form>
-          
-         
-         
-       
+
+
+
+
       </main>
     </div>
   </div>
-    
+
   <script src="js/dashboard.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/feather.min.js"></script>
@@ -110,4 +114,5 @@
     feather.replace()
   </script>
 </body>
+
 </html>
