88c1bc5b87efcd867520219ef2f8f131b3223128
KrimssmirK <krimssmirk003@gmail.com>
2023-03-02 16:03:02 +0800

    fixed error in delete_account.php

--- delete_account.php
+++ delete_account.php
@@ -1,18 +1,18 @@
 <?php
 
-  $id = $_POST['id'];
-  include_once 'config.php';
-  
-  $sql = "DELETE FROM $tblaccounts WHERE ID = $id";
-  
-  if (mysqli_query($conn, $sql)) {
-    // success
-    header("Location: http://localhost:8888/ui_manage_account.php");
-    die();
-  } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-  }
-  
-  mysqli_close($conn);
+$id = $_POST['id'];
+include_once 'config.php';
+
+$sql = "DELETE FROM $tbl_accounts WHERE ID = $id";
+
+if (mysqli_query($conn, $sql)) {
+  // success
+  header("Location: http://localhost:8888/ui_manage_account.php");
+  die();
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 
 ?>
