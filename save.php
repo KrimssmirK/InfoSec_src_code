04800b718ad45c1c20ebca5967760c95f136ca20
KrimssmirK <krimssmirk003@gmail.com>
2023-03-04 22:39:30 +0800

    added password confirmation in save.php

--- save.php
+++ save.php
@@ -5,10 +5,16 @@ require "custom_functions.php";
 $id = $_POST['id'];
 $name = $_POST['name'];
 $password = $_POST['password'];
+$confirm_password = $_POST['confirm_password'];
 
 $validated_name;
 $validated_password;
 if (isset($password)) {
+
+    if ($password != $confirm_password) {
+        echo "<script>alert('match the password and confirm password');</script>";
+        enter_page("admin_account");
+    }
     $validated_name = validate($name, "name");
     $validated_password = validate($password, "password");
 
@@ -19,10 +25,5 @@ if (isset($password)) {
     // update an account in database
     update_account($id, $validated_name, null);
 }
-
-// validate
-
-
-
 
 ?>
